9686a35af4b938fd1e2799adf1799149575b5cb9
Mauro <72555322+Mauro1986@users.noreply.github.com>
2021-06-10 10:56:28 +0200

    Add project files.

---
+++ MvcEFCoreOverview/Data/BookContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcEFCoreOverview.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcEFCoreOverview.Data
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LAPTOP-AO42BIR6;Database=BookEF;Integrated=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ MvcEFCoreOverview/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcEFCoreOverview.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
